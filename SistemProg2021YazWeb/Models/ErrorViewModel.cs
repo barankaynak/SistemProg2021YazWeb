642dcaeaf920ddf3d7ac6f0065fdd33897920146
Baran Kaynak <baran@barbardata.com>
2021-08-17 20:04:00 +0300

    Add project files.

---
+++ SistemProg2021YazWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemProg2021YazWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
